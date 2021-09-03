34b8ab8b4b967bd791913c712e995c883a7fa4a8
Samuel Goldenbaum <samuelgoldenbaum@gmail.com>
2021-09-02 16:52:31 -1000

    added missing 'plugins' prop to eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
-    extends: ['airbnb-typescript'],
-    parserOptions: {
-        project: './tsconfig.json',
-    },
-    rules:  {
-        'no-console': 'off',
-        'max-len': 'off',
-        'import/no-cycle': 'off',
-    },
-    ignorePatterns: ['.eslintrc.js']
+  extends: ['airbnb-typescript'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'import'
+  ],
+  rules: {
+    'no-console': 'off',
+    'max-len': 'off',
+    'import/no-cycle': 'off',
+    'react/jsx-filename-extension': 'off'
+  },
+  ignorePatterns: ['.eslintrc.js']
 };
